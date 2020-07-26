72e2ca2ecb46584c6bab601cfe365a0a79790be0
Paulo Daniel Carneiro <pdc187@gmail.com>
2020-07-26 04:27:23 -0300

    Run mix new cards

---
+++ lib/cards.ex
@@ -0,0 +1,18 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Create a deck of cards.
+
+  ## Examples
+
+      iex> Cards.create_deck()
+      ["Ace", "two", "three"]
+
+  """
+  def create_deck do
+    ["Ace", "two", "three"]
+  end
+end
